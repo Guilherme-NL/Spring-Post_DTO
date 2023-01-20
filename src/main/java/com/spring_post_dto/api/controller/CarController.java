c7d9abd0f647cc8443f69310da2ce5b458ed7dde
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2023-01-20 14:28:19 -0300

    feat add delete and put methods

--- src/main/java/com/spring_post_dto/api/controller/CarController.java
+++ src/main/java/com/spring_post_dto/api/controller/CarController.java
@@ -3,8 +3,11 @@ package com.spring_post_dto.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +33,21 @@ public class CarController {
         repository.save(new Car(req));
     }
     
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCar(@PathVariable Long id, @RequestBody CarDTO req) {
+        repository.findById(id).map(car -> {
+            car.setAnoModlo(req.anoModlo());
+            car.setDataFabricacao(req.dataFabricacao());
+            car.setFabricante(req.fabricante());
+            car.setModelo(req.modelo());
+            car.setValor(req.valor());
+
+            return repository.save(car);
+        });
+    }
 }
